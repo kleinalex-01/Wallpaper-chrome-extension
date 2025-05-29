821a8546044b5b1d7c5784295bb9354ba62d268a
kleinalex-01 <kleinalex215@gmail.com>
2025-05-29 14:30:45 +0200

    Background image set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [bgImg, setBgImg] = useState('');
+  const [imgAuthor, setImgAuthor] = useState('');
+
+  useEffect(() => {
+    axios.get("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+      .then(res => {
+        setBgImg(res.data.urls.full)
+        setImgAuthor(res.data.user.name)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },[])
+
+
+
   return (
     <>
-    
+    <div style={{backgroundImage: `url(${bgImg})`,}}
+         className="background-img">
+      <p className='author'><i class="fa-solid fa-camera"></i> {imgAuthor}</p>
+    </div>
     </>
   )
 }
